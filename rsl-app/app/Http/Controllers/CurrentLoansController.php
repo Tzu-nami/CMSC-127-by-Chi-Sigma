f77919dbf74e22df85c088f4e12be5532f5f70e7
JB <ultsjbn@gmail.com>
2025-11-03 15:44:14 +0800

    Modified web.php for pages to open.
    Current Loans page can't be opened yet.

--- rsl-app/app/Http/Controllers/CurrentLoansController.php
+++ rsl-app/app/Http/Controllers/CurrentLoansController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-class CurrentLoansDatabaseController extends Controller
+class CurrentLoansController extends Controller
 {
     public function index() {
         return Inertia::render('CurrentLoans/currentloans-index', []);
