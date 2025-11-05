f1fb7fa1545694e41ad44ce4467994643215343d
JB <ultsjbn@gmail.com>
2025-11-05 16:02:37 +0800

    Connected mysql to laravel. No data can be displayed yet.

--- rsl-app/app/Http/Controllers/BooksDatabaseController.php
+++ rsl-app/app/Http/Controllers/BooksDatabaseController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class BooksDatabaseController extends Controller
 {
     public function index() {
         return Inertia::render('BooksDatabase/books-index', []);
+    }
+
+    function books() {
+        $books = DB::select('SELECT * FROM books_data');
+        return view('books-database.books-index', ['books' => $books]);
     }
 }
