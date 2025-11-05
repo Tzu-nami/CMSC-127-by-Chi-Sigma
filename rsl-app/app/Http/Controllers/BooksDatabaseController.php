eacca6dab7e07b6546ce200b790783e746703c8a
JB <ultsjbn@gmail.com>
2025-11-05 16:25:05 +0800

    Update BooksDatabaseController.php

--- rsl-app/app/Http/Controllers/BooksDatabaseController.php
+++ rsl-app/app/Http/Controllers/BooksDatabaseController.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class BooksDatabaseController extends Controller
 {
-    public function index() {
-        return Inertia::render('BooksDatabase/books-index', []);
-    }
-
-    function books() {
+    public function index()
+    {
         $books = DB::select('SELECT * FROM books_data');
-        return view('books-database.books-index', ['books' => $books]);
+        
+        return Inertia::render('BooksDatabase/books-index', [
+            'books' => $books
+        ]);
     }
 }
