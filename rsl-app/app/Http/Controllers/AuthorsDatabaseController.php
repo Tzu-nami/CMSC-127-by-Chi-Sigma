d7400509110d28107cc82864fadf96d5792ded63
sentimelista <159030785+sentimelista@users.noreply.github.com>
2025-11-10 23:34:24 +0800

    Update AuthorsDatabaseController.php

--- rsl-app/app/Http/Controllers/AuthorsDatabaseController.php
+++ rsl-app/app/Http/Controllers/AuthorsDatabaseController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use App\Models\Authors;
 
 class AuthorsDatabaseController extends Controller
 {
-    public function index() {
-        // Fetch all authors from the database and store it
+    public function index(Request $request) {
+        
+        $query = Authors::query();
+
+        $query->when($request->input('search'), function ($query, $search) {
+            $query->where('AUTHOR_ID', 'like', "%{$search}%")
+                ->orWhere('AUTHOR_LASTNAME', 'like', "%{$search}%")
+                ->orWhere('AUTHOR_FIRSTNAME', 'like', "%{$search}%")
+                ->orWhere('AUTHOR_MIDDLEINITIAL', 'like', "%{$search}%");
+
+        }); 
+        
+        $author =$query->get();
         return Inertia::render('AuthorsDatabase/authors-index', [
-            'authors' => Authors::all(),
+            'authors' => $author,
+
+            'filters'=>$request->only(['search']),
         ]);
     }
 }
