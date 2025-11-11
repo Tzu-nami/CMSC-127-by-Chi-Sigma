438ee1cfff93ac999671a4690835b453764c9024
sentimelista <159030785+sentimelista@users.noreply.github.com>
2025-11-11 11:54:29 +0800

    Update StaffDatabaseController.php

--- rsl-app/app/Http/Controllers/StaffDatabaseController.php
+++ rsl-app/app/Http/Controllers/StaffDatabaseController.php
@@ -3,17 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use App\Models\Staff;
 
 class StaffDatabaseController extends Controller
 {
-    public function index() {
-        // Fetch all staff from the database and store it
-        $staff = Staff::all();
+    public function index(Request $request) {
+
+        $query = Staff::query();
+
+        $query->when($request->input('search'), function ($query, $search) {
+            $query->where('STAFF_ID', 'like', "%{$search}%")
+                ->orWhere('STAFF_LASTNAME', 'like', "%{$search}%")
+                ->orWhere('STAFF_FIRSTNAME', 'like', "%{$search}%")
+                ->orWhere('STAFF_MIDDLEINITIAL', 'like', "%{$search}%")
+                ->orWhere('STAFF_JOB', 'like', "%{$search}%");
+
+        }); 
+        
+        $staff =$query->get();
 
         return Inertia::render('StaffDatabase/staff-index', [
             'staff' => $staff,
+
+            'filters'=>$request->only(['search']),
         ]);
     }
 }
