11ec11b8301d62ad0d3c060f49c6214c12cc7425
sentimelista <159030785+sentimelista@users.noreply.github.com>
2025-11-11 11:51:38 +0800

    Update CurrentLoansController.php

--- rsl-app/app/Http/Controllers/CurrentLoansController.php
+++ rsl-app/app/Http/Controllers/CurrentLoansController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use App\Models\CurrentLoans;
 
 class CurrentLoansController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
+        
+        $query = CurrentLoans::query();
+
+        $query->when($request->input('search'), function ($query, $search) {
+            $query->where('TRANSACTION_ID', 'like', "%{$search}%")
+                ->orWhere('BORROWER_ID', 'like', "%{$search}%")
+                ->orWhere('STAFF_ID', 'like', "%{$search}%")
+                ->orWhere('BOOK_ID', 'like', "%{$search}%");
+
+        }); 
+        
+        $currentLoans =$query->get();
         return Inertia::render('CurrentLoans/currentloans-index', [
-            'currentLoans' => CurrentLoans::all(),
+            'currentLoans' => $currentLoans,
+
+            'filters'=>$request->only(['search']),
         ]);
     }
 }
